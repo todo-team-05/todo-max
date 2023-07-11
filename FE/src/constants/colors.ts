a953c0988dabc60f6b6f8167b6e2fc177c2f8249
bakhacode <bakhamitai@naver.com>
2023-07-11 14:24:00 +0900

    style: 기본 디자인 요소 세팅

---
+++ FE/src/constants/colors.ts
@@ -0,0 +1,76 @@
+const gray900 = "#14142B";
+const gray800 = "#3A3A4E";
+const gray700 = "#4E4B66";
+const gray500 = "#A0A3BD";
+const gray50 = "#FEFEFE";
+const gray100 = "#F7F7FC";
+const gray200 = "#EFF0F6";
+const red = "#FF3B30";
+const blue = "#007AFF";
+
+export const colors = {
+  gray50,
+  gray100,
+  gray200,
+  gray300: "#D0DBE0",
+  gray400: "#BEC1D5",
+  gray500,
+  gray600: "#6E7191",
+  gray700,
+  gray800,
+  gray900,
+  blue,
+  navy: "#0025E6",
+  red,
+  textStrong: gray900,
+  textBold: gray800,
+  textDefault: gray700,
+  textWeak: gray500,
+  textWhiteDefault: gray50,
+  textWhiteWeak: gray100,
+  textBrand: blue,
+  textDanger: red,
+  surfaceDefault: gray50,
+  surfaceAlt: gray100,
+  surfaceBrand: blue,
+  borderDefault: gray200,
+};
+
+// export const colorSystem = {
+//   textStrong: css`
+//     color: ${color.gray900};
+//   `,
+//   textBold: css`
+//     color: ${color.gray800};
+//   `,
+//   textDefault: css`
+//     color: ${color.gray700};
+//   `,
+//   textWeak: css`
+//     color: ${color.gray600};
+//   `,
+//   textWhiteDefault: css`
+//     color: ${color.gray50};
+//   `,
+//   textWhiteWeak: css`
+//     color: ${color.gray100};
+//   `,
+//   textBrand: css`
+//     color: ${color.blue};
+//   `,
+//   textDanger: css`
+//     color: ${color.red};
+//   `,
+//   surfaceDefault: css`
+//     background-color: ${color.gray50};
+//   `,
+//   surfaceAlt: css`
+//     background-color: ${color.gray100};
+//   `,
+//   surfaceBrand: css`
+//     background-color: ${color.blue};
+//   `,
+//   borderDefault: css`
+//     border-color: ${color.gray200};
+//   `,
+// };
