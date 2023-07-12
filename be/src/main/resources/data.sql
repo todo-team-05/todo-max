3a8cdf1862c36c77d0fbc678e7a00fafad92dad2
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-12 16:33:16 +0900

    build: DB 환경 조성
    docker를 이용하여 mysql 컨테이너를 만들고 db의 스키마를 만들었습니다.

---
+++ be/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (name) values ('해야 할 일');
+INSERT INTO category (name) values ('하고 있는 일');
+INSERT INTO category (name) values ('완료한 일');
+
+INSERT INTO action (value) values ('생성');
+INSERT INTO action (value) values ('수정');
+INSERT INTO action (value) values ('이동');
+INSERT INTO action (value) values ('삭제');
