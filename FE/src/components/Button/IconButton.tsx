38335bcc6d03bd232b6f3cd42cd5c169af51762c
qkdflrgs <qkdflrgs@gmail.com>
2023-07-13 14:58:41 +0900

    refact: icon button 컴포넌트 수정
    - icon button 컴포넌트 props 추가: width, height, color
    - SVG 인라인 컴포넌트화

---
+++ FE/src/components/Button/IconButton.tsx
@@ -0,0 +1,40 @@
+import { CloseIcon } from "./CloseIcon";
+import { EditIcon } from "./EditIcon";
+import { HistoryIcon } from "./HistoryIcon";
+import { PlusIcon } from "./PlusIcon";
+
+type Props = {
+  type: "close" | "edit" | "plus" | "history";
+  width: string;
+  height: string;
+  color: string;
+};
+
+export function IconButton({ type, width, height, color }: Props) {
+  return (
+    <button
+      css={{
+        padding: "0px",
+        display: "flex",
+        width: width,
+        height: height,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "transparent",
+        border: "none",
+      }}>
+      {type === "close" && (
+        <CloseIcon width={width} height={height} color={color} />
+      )}
+      {type === "edit" && (
+        <EditIcon width={width} height={height} color={color} />
+      )}
+      {type === "history" && (
+        <HistoryIcon width={width} height={height} color={color} />
+      )}
+      {type === "plus" && (
+        <PlusIcon width={width} height={height} color={color} />
+      )}
+    </button>
+  );
+}
