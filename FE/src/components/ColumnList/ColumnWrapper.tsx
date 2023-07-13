4130e5ca5dfcacc35f97f74cd358ea00a05ff0a9
bakhacode <bakhamitai@naver.com>
2023-07-13 11:30:28 +0900

    [FE] #4: 칼럼 영역 Mock Data 사용해서 UI 구현

--- FE/src/components/ColumnList/ColumnWrapper.tsx
+++ FE/src/components/ColumnList/ColumnWrapper.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import { Card, Column } from "../../pages/MainPage";
+import { DefaultCard } from "../Card/Card";
 import { ColumnTitle } from "./ColumnTitle";
 
-export function ColumnWrapper() {
+export function ColumnWrapper({ column }: { column: Column }) {
+  const [cardsList, setCardsList] = useState<Card[]>(column.cards); // column.cards
+  const columnTitle = column.name;
+  const cardsCount = column.cards.length;
+
+  // const cardsCount = 10; // 두자리 수 모킹 값
+
   return (
     <div
       css={{
         display: "flex",
+        flexDirection: "column",
         width: "268px",
         gap: "8px",
         padding: "0 16px",
       }}>
-      <ColumnTitle />
+      <ColumnTitle columnTitle={columnTitle} cardsCount={cardsCount} />
+      {cardsList.map((card) => (
+        <DefaultCard
+          key={card.id}
+          cardTitle={card.title}
+          cardContent={card.contents}
+        />
+      ))}
     </div>
   );
 }
