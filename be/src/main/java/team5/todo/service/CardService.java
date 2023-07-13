b791b95c121c51a0976d923ecff6ef5eb2cc9a22
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-13 16:59:43 +0900

    [BE] feat: 카드 생성 기능 구현 (#35)
    * refactor: static 메소드 생성
    
    Builder 패턴의 가독성을 위해 Builder를 반환하는 builder메서드를 생성했습니다.
    
    issue: #20
    
    * feat: 카드 생성 기능 구현
    
    issue: #20
    
    * test: 카드 생성 기능 테스트 작성
    
    빠른 구현을 위해 repository 테스트만 구현했습니다.
    
    issue: #20

---
+++ be/src/main/java/team5/todo/service/CardService.java
@@ -0,0 +1,19 @@
+package team5.todo.service;
+
+import org.springframework.stereotype.Service;
+import team5.todo.controller.dto.CardSaveRequest;
+import team5.todo.repository.CardRepository;
+
+@Service
+public class CardService {
+
+    private final CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public void save(CardSaveRequest cardSaveRequest) {
+        cardRepository.save(cardSaveRequest.toCard());
+    }
+}
