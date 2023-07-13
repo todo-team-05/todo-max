b2dd07f9eb075e84c07f848d9d91c97a00b9f070
qkdflrgs <qkdflrgs@gmail.com>
2023-07-13 14:01:37 +0900

    feat #29: MSW mock 서버 구현

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { worker } from "./mocks/browers.ts";
 import "/src/styles/main.css";
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
