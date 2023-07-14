b64c26e4aa97224e747042bcc736f256e22ebaeb
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-07-14 12:19:58 +0900

    [FE] feat: 카드 삭제 기능 (#42)
    * feat: 사용자 기기 확인 기능 구현
    
    * feat: 카드 삭제 기능 구현
    
    * design: CSS box-shadow 상수 추가

--- FE/src/contexts/ModalContext.tsx
+++ FE/src/contexts/ModalContext.tsx
@@ -2,6 +2,12 @@ import { createContext, useState, ReactNode } from "react";
 import { HistoryItemData } from "../components/Header/Header";
 
 type ModalContextType = {
+  removeCardState: boolean;
+  setRemoveCardState: React.Dispatch<React.SetStateAction<boolean>>;
+  alertType: "removeCard" | "removeHistory";
+  setAlertType: React.Dispatch<
+    React.SetStateAction<"removeCard" | "removeHistory">
+  >;
   isAlertOpen: boolean;
   setIsAlertOpen: React.Dispatch<React.SetStateAction<boolean>>;
   historyData: HistoryItemData[];
@@ -17,10 +23,17 @@ export const ModalContext = createContext<ModalContextType | undefined>(
 );
 
 export function ModalProvider({ children }: ModalProviderProps) {
+  const [removeCardState, setRemoveCardState] = useState<boolean>(false);
+  const [alertType, setAlertType] = useState<"removeCard" | "removeHistory">(
+    "removeHistory"
+  );
   const [isAlertOpen, setIsAlertOpen] = useState(false);
   const [historyData, setHistoryData] = useState<HistoryItemData[]>([]);
-
   const value: ModalContextType = {
+    removeCardState,
+    setRemoveCardState,
+    alertType,
+    setAlertType,
     isAlertOpen,
     setIsAlertOpen,
     historyData,
