562d80fac38bb6d52dc33887f0996d875571b87f
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-17 18:01:48 +0900

    feat: 카드 이동 기능 구현
    issue: #21

---
+++ be/src/main/java/team5/todo/controller/dto/CardMoveRequest.java
@@ -0,0 +1,77 @@
+package team5.todo.controller.dto;
+
+public class CardMoveRequest {
+
+    private final Long id;
+    private final Long categoryId;
+    private final Long beforeCardId;
+    private final Long afterCardId;
+
+    public static class Builder {
+
+        private Long id;
+        private Long categoryId;
+        private Long beforeCardId;
+        private Long afterCardId;
+
+        private Builder() {}
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder categoryId(Long categoryId) {
+            this.categoryId = categoryId;
+            return this;
+        }
+
+        public Builder beforeCardId(Long beforeCardId) {
+            this.beforeCardId = beforeCardId;
+            return this;
+        }
+
+        public Builder afterCardId(Long afterCardId) {
+            this.afterCardId = afterCardId;
+            return this;
+        }
+
+        public CardMoveRequest build() {
+            return new CardMoveRequest(this);
+        }
+    }
+
+    private CardMoveRequest(Long id, Long categoryId, Long beforeCardId, Long afterCardId) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.beforeCardId = beforeCardId;
+        this.afterCardId = afterCardId;
+    }
+
+    private CardMoveRequest(Builder builder) {
+        this.id = builder.id;
+        this.categoryId = builder.categoryId;
+        this.beforeCardId = builder.beforeCardId;
+        this.afterCardId = builder.afterCardId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public Long getBeforeCardId() {
+        return beforeCardId;
+    }
+
+    public Long getAfterCardId() {
+        return afterCardId;
+    }
+}
--- be/src/main/java/team5/todo/repository/CardRepository.java
+++ be/src/main/java/team5/todo/repository/CardRepository.java
@@ -1,8 +1,6 @@
 package team5.todo.repository;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import javax.sql.DataSource;
 
@@ -10,12 +8,13 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import team5.todo.controller.dto.CardMoveRequest;
 import team5.todo.domain.Card;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 @Repository
 public class CardRepository {
-	private static final double gapValue = 1000D;
+	private static final double GAP_VALUE = 1000D;
 	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 	public CardRepository(DataSource dataSource) {
@@ -46,7 +45,7 @@ public class CardRepository {
 		params.addValue("categoryId", card.getCategoryId());
 		params.addValue("title", card.getTitle());
 		params.addValue("contents", card.getContents());
-		params.addValue("gapValue", gapValue);
+		params.addValue("gapValue", GAP_VALUE);
 
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 		namedParameterJdbcTemplate.update(sql, params, keyHolder);
@@ -60,13 +59,13 @@ public class CardRepository {
 		return namedParameterJdbcTemplate.queryForObject(sql, params, cardRowMapper());
 	}
 
-	public void delete(Card card){
+	public void delete(final long id) {
 		String sql = "DELETE FROM card WHERE id = :id";
-		MapSqlParameterSource params = new MapSqlParameterSource("id", card.getId());
+		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
 		namedParameterJdbcTemplate.update(sql, params);
 	}
 
-	public void modify(Card card){
+	public void modify(Card card) {
 		String sql = "UPDATE card SET title = :title , contents = :contents WHERE id = :id";
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue("title", card.getTitle());
@@ -75,4 +74,92 @@ public class CardRepository {
 
 		namedParameterJdbcTemplate.update(sql, params);
 	}
+
+	public void moveWithBothCards(CardMoveRequest cardMoveRequest) {
+		String sql = "SELECT position from card where id = :beforeId OR id = :afterId";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("beforeId", cardMoveRequest.getBeforeCardId());
+		params.addValue("afterId", cardMoveRequest.getAfterCardId());
+
+		List<Double> positions = namedParameterJdbcTemplate.queryForList(sql, params, Double.class);
+		double avgPosition = positions.stream()
+				.mapToDouble(Double::doubleValue)
+				.average()
+				.getAsDouble();
+		sql = "UPDATE card SET position = :avgPosition, category_id = :categoryId  WHERE id = :id";
+		params.addValue("avgPosition", avgPosition);
+		params.addValue("id", cardMoveRequest.getId());
+		params.addValue("categoryId", cardMoveRequest.getCategoryId());
+
+		namedParameterJdbcTemplate.update(sql, params);
+	}
+
+	public void moveWithAfterCard(CardMoveRequest cardMoveRequest) {
+		String sql = "SELECT position from card where category_id = :categoryId AND id = :afterId";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("categoryId", cardMoveRequest.getCategoryId());
+		params.addValue("afterId", cardMoveRequest.getAfterCardId());
+		Double position = namedParameterJdbcTemplate.queryForObject(sql, params, Double.class) / 2;
+
+		sql = "UPDATE card SET position = :position, category_id = :categoryId  WHERE id = :id";
+		params.addValue("position", position);
+		params.addValue("id", cardMoveRequest.getId());
+
+		namedParameterJdbcTemplate.update(sql, params);
+	}
+
+	public void moveWithBeforeCard(CardMoveRequest cardMoveRequest) {
+		String sql = "SELECT position FROM card WHERE category_id = :categoryId AND id = :beforeId";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("categoryId", cardMoveRequest.getCategoryId());
+		params.addValue("beforeId", cardMoveRequest.getBeforeCardId());
+		Double position = namedParameterJdbcTemplate.queryForObject(sql, params, Double.class) + GAP_VALUE;
+
+		sql = "UPDATE card SET position = :position, category_id = :categoryId  WHERE id = :id";
+		params.addValue("position", position);
+		params.addValue("id", cardMoveRequest.getId());
+
+		namedParameterJdbcTemplate.update(sql, params);
+	}
+
+	public void moveWoBothCard(CardMoveRequest cardMoveRequest) {
+		String sql = "UPDATE card SET category_id = :categoryId , position = :position WHERE id = :id";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("categoryId", cardMoveRequest.getCategoryId());
+		params.addValue("position", GAP_VALUE);
+		params.addValue("id", cardMoveRequest.getId());
+
+		namedParameterJdbcTemplate.update(sql, params);
+	}
+
+	public static double getGapValue() {
+		return GAP_VALUE;
+	}
+
+	/*
+	## 앞과 뒤 카드 아이디 없으면 어떻게 position 넣을 것인지? 어디에서 처리할 것인지
+	위 (앞): 최신
+	카드
+	아래 (뒤): 과거
+
+	앞 X 뒤 X: 레포지토리에서 갭 넣음
+	앞 O 뒤 X: 앞 카드의 포지션을 가지고 와서 그 값의 절반을 넣습니다.
+	앞 X 뒤 O: 뒤 카드의 포지션을 가지고 와서 그 값에 갭밸류를 더해서 넣습니다.
+	앞 O 뒤 O: 앞, 뒤 포지션/2해서 넣어줍니다.
+
+	// 변경사항
+	앞 O 뒤 X: 카드 테이블에서 동일한 카테고리의 포지션 값들중 최솟값을 가지고 와서 그 값의 절반을 넣습니다.
+	앞 X 뒤 O: 카드 테이블에서 동일한 카테고리의 포지션 값들중 최댓값을 가지고 와서 그 값에 갭밸류를 더해 넣습니다.
+	->
+	앞 O 뒤 X: 앞 카드의 포지션을 가지고 와서 그 값의 절반을 넣습니다.
+	앞 X 뒤 O: 뒤 카드의 포지션을 가지고 와서 그 값에 갭밸류를 더해서 넣습니다.
+	변경 사유:
+	이전 방법에서는 DB에서 값을 다 탐색하고 최댓값, 최솟값을 가져와야 하는데 그것보다 아이디로 탐색하는게 시간이 적게 걸릴 것 같습니다.
+	또한 이전 방법에서는 아이디가 필요 없는데 바꾼 방법에서는 아이디를 사용합니다.
+
+	X
+	카드
+	최신
+	 */
+
 }
