d95ce8c897e78fa9990e69c699fef24f061552ba
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-17 23:54:59 +0900

    [BE] feat: 카드 이동 기능 구현 (#21) (#46)
    * feat: 카드 이동 기능 구현
    
    issue: #21
    
    * test: 카드 이동 테스트 코드 작성
    
    카드 이동 테스트 코드를 작성했습니다.
    또한 Controller 테스트 코드를 작성해서 통신의 형식이 제대로 의도한 형식인지 확인했습니다.
    
    issue: #21

--- be/src/main/java/team5/todo/controller/CardController.java
+++ be/src/main/java/team5/todo/controller/CardController.java
@@ -1,7 +1,8 @@
 package team5.todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import team5.todo.controller.dto.CardDeleteRequest;
+import team5.todo.controller.dto.CardMoveRequest;
 import team5.todo.controller.dto.CardSaveRequest;
 import team5.todo.controller.dto.CardModifyRequest;
 import team5.todo.service.CardService;
@@ -12,6 +13,7 @@ public class CardController {
 
     private final CardService cardService;
 
+    @Autowired
     public CardController(CardService cardService) {
         this.cardService = cardService;
     }
@@ -21,13 +23,18 @@ public class CardController {
         cardService.save(cardSaveRequest);
     }
     
-    @DeleteMapping
-    public void delete(@RequestBody final CardDeleteRequest cardDeleteRequest) {
-        cardService.delete(cardDeleteRequest);
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable final long id) {
+        cardService.delete(id);
     }
 
     @PutMapping("/modify")
     public void modify(@RequestBody final CardModifyRequest cardModifyRequest) {
         cardService.modify(cardModifyRequest);
+    }
+
+    @PutMapping("/move")
+    public void move(@RequestBody final CardMoveRequest cardMoveRequest) {
+        cardService.move(cardMoveRequest);
     }
 }
---
+++ be/src/test/java/team5/todo/annotation/ServiceTest.java
@@ -0,0 +1,15 @@
+package team5.todo.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(MockitoExtension.class)
+public @interface ServiceTest {
+
+}
