6e88f1bdb8be62981982e6dbc32b3c83bd9e39fa
qkdflrgs <qkdflrgs@gmail.com>
2023-07-18 18:48:57 +0900

    refact #47: CSS 코드 분리

--- FE/src/components/Dim/Dim.tsx
+++ FE/src/components/Dim/Dim.tsx
@@ -1,15 +1,13 @@
 export function Dim() {
-  return (
-    <div
-      css={{
-        position: "fixed",
-        top: "0",
-        left: "0",
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "#14212B4D",
-        zIndex: 4,
-      }}
-    />
-  );
+  return <div css={dimWrapper} />;
 }
+
+const dimWrapper = {
+  position: "fixed" as const,
+  top: "0",
+  left: "0",
+  width: "100vw",
+  height: "100vh",
+  backgroundColor: "#14212B4D",
+  zIndex: 4,
+};
